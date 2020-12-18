3e8087d78c4b775bbb8434496f18840431637808
wrlu <luweirao@outlook.com>
2020-12-18 18:02:51 +0800

    Update apple.py

--- apple.py
+++ apple.py
@@ -6,10 +6,15 @@ def get_latest_security_update():
     response = __send_request__('/')
     if response.status_code == 200:
         result = response.text
-        ul_list = re.findall('iOS 和 iPadOS 的最新版本是 (.+?)。', result)
+        ul_list = re.findall('The latest version of iOS and iPadOS is (.+?)\.&nbsp;Learn', result)
+        if len(ul_list) == 0 or (len(ul_list) == 1 and ul_list[0].find('and') != -1):
+            ul_list = re.findall('The latest version of iOS and iPadOS is (.+?), and', result)
+        if len(ul_list) == 0:
+            print("Unable to parse ul_list.")
         latest_ver = ul_list[0]
-        link_no_cve_re = '<td>iOS {0} 和 iPadOS {0}<br>\\s+<span class="note">(.+?)</span></td>'.format(latest_ver)
-        link_cve_re = '<td><a href="(.+?)">iOS {0} 和 iPadOS {0}</a></td>'.format(latest_ver)
+        print("Latest iOS version is "+latest_ver)
+        link_no_cve_re = '<td>iOS {0} and iPadOS {0}<br>\\s+<span class="note">(.+?)</span></td>'.format(latest_ver)
+        link_cve_re = '<td><a href="(.+?)">iOS {0} and iPadOS {0}</a></td>'.format(latest_ver)
         td_list = re.findall(link_no_cve_re, result)
         if len(td_list) == 1:
             latest_link = td_list[0]
@@ -21,7 +26,7 @@ def get_latest_security_update():
 
 
 def __send_request__(path: str):
-    base_url = 'https://support.apple.com/zh-cn/HT201222'
+    base_url = 'https://support.apple.com/en-us/HT201222'
     response = requests.get(base_url + path)
     return response
 
